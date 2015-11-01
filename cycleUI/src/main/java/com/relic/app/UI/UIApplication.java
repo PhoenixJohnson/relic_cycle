96fd2716b905f05549240683a1d956c5518f0f7b
PhoenixJohnson <yosku@163.com>
2015-11-01 09:36:07 +0800

    add meta info

--- cycleUI/src/main/java/com/relic/app/UI/UIApplication.java
+++ cycleUI/src/main/java/com/relic/app/UI/UIApplication.java
@@ -27,7 +27,7 @@ public class UIApplication {
         SpringApplication springApplication =
                 new SpringApplication(Application.class);
         springApplication.addListeners(
-                new ApplicationPidListener("/run/jenkins/cycleUI.pid"));
+                new ApplicationPidListener("/relicRun/cycleUI.pid"));
         springApplication.run(UIApplication.class, args);
     }
 
